1cd132944e57bed2864b6d31a33187050e7214db
Ankesh Chopde <acchopde84@gmail.com>
2023-10-09 20:12:30 +0530

    backend setup done

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,15 @@
-const app = require("express")
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(8000,async()=>{
+try{
+   
+    console.log("Database connection established")
+}catch(e){
+    console.log(e.message)
+}
+console.log(`Server is live at port ${8080}`)
+})
